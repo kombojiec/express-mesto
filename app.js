455d5ad6a2b94bf34dcd0c16d1a90bebe6c5ef2c
Kulakov Pavel <kombojiec@gmail.com>
2021-01-28 00:26:47 +0300

    feat(mongo): install mongo & create card/user models

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const path = require ('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 const {PORT = 3000} = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ models/cards.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose');
+
+const cardSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    required: true,
+    type: String,
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'UserSchema',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    default: [],
+  }],
+  created: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+module.exports = model('card', cardSchema);
